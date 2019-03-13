2a75b778ce6f2c62859619dd6a907c5e9ccdfe6c
Marcus Lundgren <lundgren90@linuxmail.org>
2019-03-13 20:21:23 +0100

    added searchbar with api integrated to it

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,28 @@
 import React from "react";
-import SearchBar from "./SearchBar.js";
+import unsplash from "../api/unsplash";
+import SearchBar from "./SearchBar";
+import ImageList from "./ImageList";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
-  }
+  state = {
+    images: []
+  };
+  //---------------------------------//
+  onSearchSubmit = async term => {
+    const response = await unsplash.get("/search/photos", {
+      params: { query: term }
+    });
+    console.log(this);
+    this.setState({ images: response.data.results });
+    //sparar resultatet i state
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        <ImageList images={this.state.images} />
+        {/*onSubmit kan kallas vad som helst. Vi skickar ner funktionen här till SearchBar Komponenten */}
       </div>
     );
   }
