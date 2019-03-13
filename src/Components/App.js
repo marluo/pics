93e539b3340e8336ebd718e0c33a360846e4abb8
Marcus Lundgren <lundgren90@linuxmail.org>
2019-03-13 22:03:22 +0100

    pics now searchable and scaling correctly

--- src/Components/App.js
+++ src/Components/App.js
@@ -7,22 +7,24 @@ class App extends React.Component {
   state = {
     images: []
   };
-  //---------------------------------//
+  //state
   onSearchSubmit = async term => {
+    //async function
     const response = await unsplash.get("/search/photos", {
+      //vi sparar arrayen med bilder i response
       params: { query: term }
     });
-    console.log(this);
     this.setState({ images: response.data.results });
-    //sparar resultatet i state
+    //sparar resultatet av bilderna i state
   };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        {/* vi skickar ner värdet av det vi söker på i onSearchSubmit*/}
         <ImageList images={this.state.images} />
-        {/*onSubmit kan kallas vad som helst. Vi skickar ner funktionen här till SearchBar Komponenten */}
+        {/*onSubmit kan kallas vad som helst. Vi skickar ner funktionen här som prop till SearchBar Komponenten */}
       </div>
     );
   }
